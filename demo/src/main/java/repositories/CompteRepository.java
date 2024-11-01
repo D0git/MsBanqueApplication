2f8168e43293eae87fae44f0aa143cfb934ba39e
dohatir <dohagit0@gmail.com>
2024-11-01 21:32:13 +0100

    msbanqueapplication

--- demo/src/main/java/repositories/CompteRepository.java
+++
@@ -1,7 +0,0 @@
-package repositories;
-
-import entities.Compte;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompteRepository extends JpaRepository<Compte,Long> {
-}
